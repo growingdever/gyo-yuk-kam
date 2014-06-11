7c6e6db8cd38a9d1bcaf53ab559a780c297a5bab
loki <growingdever@gmail.com>
2014-06-11 21:24:46 +0900

    연임 성공, 실패 다이얼로그 프리팹 추가.

---
+++ Assets/Scripts/YeonimFail.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YeonimFail : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickButtonExit() {
+		// go to title scene
+	}
+}
---
+++ Assets/Scripts/YeonimSuccess.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YeonimSuccess : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickButtonExit() {
+		// close dialog and play continually
+		Destroy (gameObject);
+	}
+}
