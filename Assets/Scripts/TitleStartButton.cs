cd48b2e2ae26ed0b2006e63da31a6d93b0c09593
loki <growingdever@gmail.com>
2014-06-13 04:24:53 +0900

    타이틀 화면 튜토리얼 추가.

--- Assets/Scripts/TitleStartButton.cs
+++ Assets/Scripts/TitleStartButton.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class TitleStartButton : MonoBehaviour {
 
+	public UITexture _tutorialTexture;
+	int _curr;
+
 	// Use this for initialization
 	void Start () {
-	
+		NGUITools.SetActive (_tutorialTexture.gameObject, false);
 	}
 	
 	// Update is called once per frame
@@ -14,6 +17,18 @@ public class TitleStartButton : MonoBehaviour {
 	}
 
 	public void OnClickStartButton() {
-		Application.LoadLevel(1);
+		NextTutorial ();
+	}
+
+	public void NextTutorial() {
+		NGUITools.SetActive (_tutorialTexture.gameObject, true);
+
+		_curr++;
+		if (_curr > 5) {
+			Application.LoadLevel (1);
+		}
+
+		Texture tex = Resources.Load ("Images/Tu" + _curr) as Texture;
+		_tutorialTexture.mainTexture = tex;
 	}
 }
