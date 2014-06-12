a60a589fde3079059f64203d13d2bf9889e0b771
loki <growingdever@gmail.com>
2014-06-13 01:45:22 +0900

    엔딩신 transition 스크립 커밋.

---
+++ Assets/Scripts/EndingLabelTransition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingLabelTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Tweening ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Tweening() {
+		TweenAlpha.Begin (gameObject, 3.0f, 1.0f);
+		yield return new WaitForSeconds (3.0f);
+
+		TweenAlpha.Begin (gameObject, 3.0f, 0.0f);
+		yield return new WaitForSeconds (3.0f);
+
+		Application.LoadLevel (0);
+	}
+}
