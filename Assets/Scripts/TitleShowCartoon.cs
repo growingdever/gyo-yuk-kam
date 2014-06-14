706937a4fe1349171f0387c4d6e4a7e45f20ce75
JungJuHong <junghong456@gmail.com>
2014-06-14 19:45:05 +0900

    처음에 만화 보여줌.

---
+++ Assets/Scripts/TitleShowCartoon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleShowCartoon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnFinished() {
+		StartCoroutine( ChangeScene() );
+	}
+
+	IEnumerator ChangeScene() {
+		yield return new WaitForSeconds( 3.0f );
+		Application.LoadLevel( 0 );
+	}
+}
